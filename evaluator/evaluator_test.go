b8419d6577b6ef8594b4cbdeae2e1151511b5bc1
Arafat Hasan <nayemarafat786@gmail.com>
2026-03-08 23:58:05 +0600

    Added Boolean evaluation expression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nayemmmmmmmmmm/yanma/parser"
 )
 
+// Testing Integer Evaluator
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -31,7 +32,6 @@ func testEval(input string) object.Object {
 	return Eval(program)
 }
 
-
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	result, ok := obj.(*object.Integer)
 	if !ok {
@@ -44,4 +44,34 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 		return false
 	}
 	return true
-} 
+}
+
+// Testing Boolean Evaluator
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
